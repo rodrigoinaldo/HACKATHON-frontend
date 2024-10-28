150dd59fcd0e725dfd8b577c3204048182c24d4d
gabriel7419 <sgtgabriel7419@gmail.com>
2024-10-28 15:08:34 -0300

    Corrigido os problemas de import dentro dos arquivos "TableTwo.tsx" e "TableOne.tsx", que prejudicavam a execução correta do programa.

--- src/components/Tables/tables/TableOne.tsx
+++ src/components/Tables/tables/TableOne.tsx
@@ -1,9 +1,9 @@
-import { BRAND } from '../../types/brand';
-import BrandOne from '../../images/brand/brand-01.svg';
-import BrandTwo from '../../images/brand/brand-02.svg';
-import BrandThree from '../../images/brand/brand-03.svg';
-import BrandFour from '../../images/brand/brand-04.svg';
-import BrandFive from '../../images/brand/brand-05.svg';
+import { BRAND } from '../../../types/brand';
+import BrandOne from '../../../images/brand/brand-01.svg';
+import BrandTwo from '../../../images/brand/brand-02.svg';
+import BrandThree from '../../../images/brand/brand-03.svg';
+import BrandFour from '../../../images/brand/brand-04.svg';
+import BrandFive from '../../../images/brand/brand-05.svg';
 
 const brandData: BRAND[] = [
   {
