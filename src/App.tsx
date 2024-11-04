87cfaad333d9ffcc7301c7aa6e107e8e65d584df
Rodrigo Alcantara <rodrigoinaldo281@gmail.com>
2024-11-03 21:39:12 -0300

    ggg

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ function App() {
           path="/profile"
           element={
             <ProtectedRoute>
-              <PageTitle title="Profile" />
+              <PageTitle title="Profyile" />
               <Profile />
             </ProtectedRoute>
           }
